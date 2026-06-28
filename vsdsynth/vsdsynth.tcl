fa05b90dd0618f212ff31a35340a32595f1f9cd5
Irene Raphael <72806031+Irene-ux@users.noreply.github.com>
2026-06-28 22:35:12 +0000

    Day 2 complete: variable creation and constraints processing

---
+++ vsdsynth/vsdsynth.tcl
@@ -0,0 +1,130 @@
+#!/bin/env tclsh
+
+#    Checks whether vsdsynth usage is correct or not        #
+#                                                           #
+
+set enable_prelayout_timing 1
+set working_dir [exec pwd]
+set vsd_array_length [llength [split [lindex $argv 0] .]]
+set input [lindex [split [lindex $argv 0] .] $vsd_array_length-1]
+
+if {![regexp {csv} $input] || $argc!=1} {
+    puts "Error in usage"
+    puts "Usage: ./vsdsynth <.csv>"
+    puts "where <.csv> file has below inputs"
+    exit
+} else {
+
+#--------------------------------------------------------------------#
+# converts csv to matrix and creates initial variables               #
+# DesignName OutputDirectory NetlistDirectory EarlyLibraryPath       #
+# LateLibraryPath ConstraintsFile                                    #
+# If you are modifying this script, please use above variables as    #
+# starting point. Use 'puts' command to report above variables       #
+#--------------------------------------------------------------------#
+
+    set filename [lindex $argv 0]
+    package require csv
+    package require struct::matrix
+    struct::matrix m
+    set f [open $filename]
+    csv::read2matrix $f m , auto
+    close $f
+    set columns [m columns]
+    m add columns $columns
+    m link my_arr
+    set num_of_rows [m rows]
+    set i 0
+
+    while {$i < $num_of_rows} {
+        puts "\nInfo: Setting $my_arr(0,$i) as '$my_arr(1,$i)'"
+        if {$i == 0} {
+            set [string map {" " ""} $my_arr(0,$i)] $my_arr(1,$i)
+        } else {
+            set [string map {" " ""} $my_arr(0,$i)] [file normalize $my_arr(1,$i)]
+        }
+        set i [expr {$i+1}]
+    }
+}
+
+puts "\nInfo: Below are the list of initial variables and their values. User can use these variables for further debug. Use 'puts \$variable_name' command to query value of below variables"
+puts "DesignName = $DesignName"
+puts "OutputDirectory = $OutputDirectory"
+puts "NetlistDirectory = $NetlistDirectory"
+puts "EarlyLibraryPath = $EarlyLibraryPath"
+puts "LateLibraryPath = $LateLibraryPath"
+puts "ConstraintsFile = $ConstraintsFile"
+
+#--------------------------------------------------------------------#
+#-----Below script checks if directories and files mentioned in csv file, exists or not-----#
+#-------------------------------------------------------------------------------------------#
+
+if {![file exists $EarlyLibraryPath]} {
+    puts "\nError: Cannot find early cell library in path $EarlyLibraryPath. Exiting..."
+    exit
+} else {
+    puts "\nInfo: Early cell library found in path $EarlyLibraryPath"
+}
+
+if {![file exists $LateLibraryPath]} {
+    puts "\nError: Cannot find late cell library in path $LateLibraryPath. Exiting..."
+    exit
+} else {
+    puts "\nInfo: Late cell library found in path $LateLibraryPath"
+}
+
+if {![file isdirectory $OutputDirectory]} {
+    puts "\nInfo: Cannot find output directory $OutputDirectory. Creating $OutputDirectory"
+    file mkdir $OutputDirectory
+} else {
+    puts "\nInfo: Output directory found in path $OutputDirectory"
+}
+
+if {![file isdirectory $NetlistDirectory]} {
+    puts "\nError: Cannot find RTL netlist directory in path $NetlistDirectory. Exiting..."
+    exit
+} else {
+    puts "\nInfo: RTL netlist directory found in path $NetlistDirectory"
+}
+
+if {![file exists $ConstraintsFile]} {
+    puts "\nError: Cannot find constraints file in path $ConstraintsFile. Exiting..."
+    exit
+} else {
+    puts "\nInfo: Constraints file found in path $ConstraintsFile"
+}
+
+#----------------------- Constraints file creations-------------------------#
+#--------------------------- SDC format ------------------------------------#
+#---------------------------------------------------------------------------#
+
+puts "\nInfo: Dumping SDC constraints for $DesignName"
+::struct::matrix constraints
+set chan [open $ConstraintsFile]
+csv::read2matrix $chan constraints , auto
+close $chan
+set number_of_rows [constraints rows]
+puts "number_of_rows = $number_of_rows"
+set number_of_columns [constraints columns]
+puts "number_of_columns = $number_of_columns"
+
+#----check row number for "clocks" and column number for "IO delays and slew section" in constraints.csv--##
+set clock_start [lindex [lindex [constraints search all CLOCKS] 0] 1]
+set clock_start_column [lindex [lindex [constraints search all CLOCKS] 0] 0]
+puts "clock_start = $clock_start"
+puts "kunal clock_start_column = $clock_start_column"
+
+#----check row number for "inputs" section in constraints.csv--##
+set input_ports_start [lindex [lindex [constraints search all INPUTS] 0] 1]
+puts "input_ports_start = $input_ports_start"
+
+#----check row number for "outputs" section in constraints.csv--##
+set output_ports_start [lindex [lindex [constraints search all OUTPUTS] 0] 1]
+puts "output_ports_start = $output_ports_start"
+return
+
+#------------------Clock constraints-------------------##
+#------------------clock latency constraints-----------##
+
+set clock_early_rise_delay_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}] early_rise_delay] 0] 0]
+set clock_early_fall_delay_start [lindex [lindex [constraints search rect $clock_start_column $clock_start [expr {$number_of_columns-1}] [expr {$input_ports_start-1}] early_fall_delay] 0] 0]
